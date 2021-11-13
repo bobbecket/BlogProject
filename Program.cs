f8dfe6f4e159be8fdbcc07eff3d2bf13beeb51d1
Robert Becket <36114621+bobbecket@users.noreply.github.com>
2021-11-13 12:34:46 -0800

    update db for heroku
    Get db to work with heroku deployment

--- Program.cs
+++ Program.cs
@@ -15,8 +15,6 @@ namespace BlogProject
     {
         public static async Task Main(string[] args)
         {
-            //CreateHostBuilder(args).Build().Run();
-
             var host = CreateHostBuilder(args).Build();
 
             var dataService = host.Services
